6cb84f0361d991c88feda176d355cb14fd704e05
calihanvrablick <cvrablick@csuchico.edu>
2023-10-26 10:06:30 -0700

    teleporter object and first game level. start of second

---
+++ PlatformerClone/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Vrablick, Calihan
+ * 10/26/23
+ * 
+ */
+
+public class Teleporter : MonoBehaviour
+{
+    //game object to tell where the spawn point is
+    public GameObject spawnPoint;
+}
