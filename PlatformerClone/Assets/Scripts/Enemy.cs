8d1b76e9603d41cae09aa2a3df633124703a91ce
calihanvrablick <cvrablick@csuchico.edu>
2023-10-30 19:42:20 -0700

    Enemy made & made into a prefab

---
+++ PlatformerClone/Assets/Scripts/Enemy.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Vrablick, Calihan
+ * 10/30/23
+ * Handles code for enemies
+ */
+
+public class Enemy : MonoBehaviour
+{
+    //game objects to determine how far left/right enemy goes
+    public GameObject leftPoint;
+    public GameObject rightPoint;
+
+    //boundary points for left/right
+    private Vector3 leftPosition;
+    private Vector3 rightPosition;
+
+    //how fast the enemy travels
+    public float speed;
+
+    //the direction it is going
+    public bool goingLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftPosition = leftPoint.transform.position;
+        rightPosition = rightPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EnemyMove();
+    }
+
+    /// <summary>
+    /// Make the enemy move left and right
+    /// </summary>
+    private void EnemyMove()
+    {
+        if (goingLeft == true)
+        {
+            //once the enemy reaches the leftPos - goingLeft is false
+            if (transform.position.x <= leftPosition.x)
+            {
+                goingLeft = false;
+            }
+            else
+            {
+                //translate the enemy left by speed using Time.deltaTime
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            //once the enemy reaches the rightPos - goingLeft is false
+            if (transform.position.x >= rightPosition.x)
+            {
+                goingLeft = true;
+            }
+            else
+            {
+                //translate the enemy by speed using Time.deltaTime
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+    }
+}
